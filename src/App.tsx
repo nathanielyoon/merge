034fb9bdaa8009a0724fc46b90b3b6256325b1ac
Nathaniel Yoon <nyoon2@jh.edu>
2023-10-17 17:35:44 -0400

    multiple rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { type Component, createSignal, Show } from "solid-js";
+import { type Component, createMemo, createSignal, For, Show } from "solid-js";
 
 const Section = ((p) => (
   <section class={`flex flex-col gap-1 ${p.class ?? ""}`}>
@@ -8,7 +8,6 @@ const Section = ((p) => (
     </p>
   </section>
 )) satisfies Component<{ title: string; content: string; class?: string }>;
-
 const Document: Component<{ fields: string[] }> = (p) => (
   <article class="flex flex-col gap-3 items-stretch">
     <Section title="Student Name" content={p.fields[0]} />
@@ -36,33 +35,30 @@ const Document: Component<{ fields: string[] }> = (p) => (
     </section>
   </article>
 );
-
 export default () => {
-  const [one, set_one] = createSignal("");
-  const [two, set_two] = createSignal("");
+  const [raw, set_raw] = createSignal("");
+  const rows = () =>
+    raw().split("\n").filter(Boolean).map((row) => row.split("\t"));
+  const [include, set_include] = createSignal(0);
+  const included = createMemo(() => {
+    let a = rows().filter((_, i) => include() & 1 << i), i = 0;
+    const I = a.length - a.length % 2, z: [string[], string[]?][] = [];
+    while (i < I) z.push([a[i++], a[i++]]);
+    return (a.length % 2 && z.push([a[i]]), z);
+  });
   return (
     <main class="px-4 py-8 flex flex-col gap-8">
       <section class="flex flex-col gap-2 print_hidden">
-        <label class="flex items-center gap-2">
-          <span class="w-12">Row 1</span>
-          <input
-            value={one()}
-            oninput={({ target }) => set_one(target.value)}
-            class="border border-black py-1 px-1.5 flex-1"
-          />
-        </label>
-        <label class="flex items-center gap-2">
-          <span class="w-12">Row 2</span>
-          <input
-            value={two()}
-            oninput={({ target }) => set_two(target.value)}
-            class="border border-black py-1 px-1.5 flex-1"
-          />
-        </label>
+        <textarea
+          oninput={({ target }) => set_raw(target.value)}
+          class="border border-black h-32"
+        >
+          {raw()}
+        </textarea>
         <button
           class="border border-black py-1 px-1.5 transition duration-75 cursor-pointer disabled_opacity-50 disabled_cursor-default enabled_bg-black enabled_text-white flex items-center gap-1 justify-center"
           onclick={() => window.print()}
-          disabled={!one()}
+          disabled={!included().length}
         >
           <svg
             width="24"
@@ -76,17 +72,54 @@ export default () => {
           >
             <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
           </svg>
-          Print
+          Print {included().length}
         </button>
-      </section>
-      <section class="grid grid-cols-2 gap-16">
-        <Show when={one()}>
-          {(value) => <Document fields={value().split("\t")} />}
+        <Show when={rows().length}>
+          <table>
+            <thead>
+              <tr class="font-normal border-b border-black">
+                <th>?</th>
+                <th class="text-left">Student</th>
+                <th class="text-left">Date</th>
+                <th class="text-left">Assessment</th>
+              </tr>
+            </thead>
+            <tbody>
+              <For each={rows()}>
+                {(row, index) => (
+                  <tr class="border-b border-gray-500">
+                    <td class="text-center">
+                      <input
+                        class="w-5 h-5 mt-1"
+                        type="checkbox"
+                        name={row[0]}
+                        value="include"
+                        oninput={({ target }) =>
+                          set_include((was) =>
+                            target.checked
+                              ? was | 1 << index()
+                              : was & ~(1 << index())
+                          )}
+                      />
+                    </td>
+                    <th scope="row" class="font-normal text-left">{row[0]}</th>
+                    <td>{row[2]}</td>
+                    <td>{row[4]}</td>
+                  </tr>
+                )}
+              </For>
+            </tbody>
+          </table>
         </Show>
-        <Show when={two()}>
-          {(value) => <Document fields={value().split("\t")} />}
-        </Show>
       </section>
+      <For each={included()}>
+        {(rows) => (
+          <section class="grid grid-cols-2 gap-16 print_break-after-page">
+            <Document fields={rows[0]} />
+            <Show when={rows[1]}>{(also) => <Document fields={also()} />}</Show>
+          </section>
+        )}
+      </For>
     </main>
   );
 };
