20887d16c0d82ff16a9f4c7ec5d916172bbb2096
Nathaniel Yoon <nyoon2@jh.edu>
2023-10-17 12:08:47 -0400

    single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tailwind from "tailwindcss";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), viteSingleFile({ removeViteModuleLoader: true })],
   server: { port: 3000 },
   css: { postcss: { plugins: [tailwind()] } },
 });
